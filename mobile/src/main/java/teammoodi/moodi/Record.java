52f5e08e609f57d2b8c8e722c9eb3100188d767c
Arthur Byra <ahb5190@zeno.cs.hbg.psu.edu>
2018-02-14 15:46:41 -0500

    Added request for permissions

--- mobile/src/main/java/teammoodi/moodi/Record.java
+++ mobile/src/main/java/teammoodi/moodi/Record.java
@@ -8,26 +8,23 @@ import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 import android.support.v4.app.ActivityCompat;
-import android.content.pm.PackageManager;
 import android.support.v4.content.ContextCompat;
 
 import java.io.IOException;
 import java.util.Random;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class Record extends AppCompatActivity{
+    //to access recording on phone: go to settings, storage, files
 
     public static final int REQUESTPERMISSION = 1;
     Random random;
     String AudioSavePathInDevice = null;
-    String audioFileName = "qazwsxedcrfvtbgynhmujilkpo";
     MediaRecorder mediaRecorder;
 
     private Button recordButton;
@@ -45,12 +42,10 @@ public class Record extends AppCompatActivity{
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 if(checkPermission()) {
 
                     AudioSavePathInDevice =
-                            Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
-                                    "AudioRecording.3gp";
+                            Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + "AudioRecording.3gp";
 
                     MediaRecorderReady();
 
@@ -66,8 +61,7 @@ public class Record extends AppCompatActivity{
                     recordButton.setEnabled(false);
                     stopButton.setEnabled(true);
 
-                    Toast.makeText(Record.this, "Recording started",
-                            Toast.LENGTH_LONG).show();
+                    Toast.makeText(Record.this, "Recording started", Toast.LENGTH_LONG).show();
                 } else {
                     requestPermission();
                 }
@@ -79,8 +73,6 @@ public class Record extends AppCompatActivity{
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //mehhhh
             }
         });
 
@@ -95,27 +87,22 @@ public class Record extends AppCompatActivity{
     }
 
     private void requestPermission() {
-        ActivityCompat.requestPermissions(MainActivity.this, new
-                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, REQUESTPERMISSION);
+        ActivityCompat.requestPermissions(Record.this, new String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, REQUESTPERMISSION);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case RequestPermissionCode:
+            case REQUESTPERMISSION:
                 if (grantResults.length> 0) {
-                    boolean StoragePermission = grantResults[0] ==
-                            PackageManager.PERMISSION_GRANTED;
-                    boolean RecordPermission = grantResults[1] ==
-                            PackageManager.PERMISSION_GRANTED;
+                    boolean StoragePermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean RecordPermission = grantResults[1] == PackageManager.PERMISSION_GRANTED;
 
                     if (StoragePermission && RecordPermission) {
-                        Toast.makeText(MainActivity.this, "Permission Granted",
+                        Toast.makeText(Record.this, "Permission Granted",
                                 Toast.LENGTH_LONG).show();
                     } else {
-                        Toast.makeText(MainActivity.this,"Permission
-                                Denied",Toast.LENGTH_LONG).show();
+                        Toast.makeText(Record.this, "Permission Denied",Toast.LENGTH_LONG).show();
                     }
                 }
                 break;
@@ -123,12 +110,9 @@ public class Record extends AppCompatActivity{
     }
 
     public boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
-                WRITE_EXTERNAL_STORAGE);
-        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
-                RECORD_AUDIO);
-        return result == PackageManager.PERMISSION_GRANTED &&
-                result1 == PackageManager.PERMISSION_GRANTED;
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
     }
 
     public void MediaRecorderReady(){
