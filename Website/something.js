9d49efa4b986e43046f3f8b5675ffda721bda143
Shreya Jacob <shreyajacob1995@gmail.com>
2018-02-15 10:17:29 -0500

    Update something.js

--- Website/something.js
+++ Website/something.js
@@ -1,4 +1,25 @@
-Holder file for git upload.
+//$ npm install pug
 
-Delete whenever you actually start using this direcotory
+//- template.pug
+p #{name}'s Pug source code!
 
+const pug = require('pug');
+
+// Compile the source code
+const compiledFunction = pug.compileFile('template.pug');
+
+// Render a set of data
+console.log(compiledFunction({
+  name: 'Timothy'
+}));
+
+// Render another set of data
+console.log(compiledFunction({
+  name: 'Forbes'
+}));
+
+const pug = require('pug');
+
+console.log(pug.renderFile('template.pug', {
+  name: 'Timothy'
+}));
