8d44525f12b8f4283bb0e82edf7d9f4355645ee4
swanj21 <swanj21@gmail.com>
2018-02-15 11:44:14 -0500

    Added capability for wear to connect to phone

--- mobile/src/main/java/teammoodi/moodi/Dashboard.java
+++ mobile/src/main/java/teammoodi/moodi/Dashboard.java
@@ -9,6 +9,11 @@ import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
 
+import com.google.android.gms.wearable.DataClient;
+import com.google.android.gms.wearable.MessageClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+
 public class Dashboard extends AppCompatActivity {
 
     private Button button2;
@@ -18,7 +23,8 @@ public class Dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        button2 = (Button) findViewById(R.id.button2);
+        
+        button2 = findViewById(R.id.button2);
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,4 +42,30 @@ public class Dashboard extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+    /* TRYING OUT THE MESSAGE PASSING BETWEEN THE WEAR AND THE PHONE
+    *
+    * Added
+    *   implements MessageClient.OnMessageReceivedListener
+    *   the code below
+    *
+    public static final String VOICE_TRANSCRIPTION_MESSAGE_PATH = "/voice_transcription";
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent)
+    {
+        if (messageEvent.getPath().equals(VOICE_TRANSCRIPTION_MESSAGE_PATH))
+        {
+            Intent startIntent = new Intent(this, Dashboard.class);
+            startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startIntent.putExtra("VOICE_DATA", messageEvent.getData());
+            startActivity(startIntent);
+        }
+    } */
+
+    // DataClient dataClient = Wearable.getDataClient(this);
+
+    // 192.168.1.176:5555
+
+
 }
--- wear/src/main/java/teammoodi/moodi/Dashboard.java
+++ wear/src/main/java/teammoodi/moodi/Dashboard.java
@@ -1,105 +1,59 @@
 package teammoodi.moodi;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.phone.PhoneDeviceType;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.CapabilityApi;
 import com.google.android.gms.wearable.CapabilityClient;
 import com.google.android.gms.wearable.CapabilityInfo;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeClient;
 import com.google.android.gms.wearable.Wearable;
 
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 public class Dashboard extends WearableActivity {
 
     private TextView mTextView;
-    public static final String VOICE_TRANSCRIPTION_MESSAGE_PATH = "/voice_transcription";
-    private static final String VOICE_TRANSCRIPTION_CAPABILITY_NAME = "voice_transcription";
-
-    private String transcriptionNodeID = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        mTextView = findViewById(R.id.text);
+
+
 
         // Enables Always-on
         setAmbientEnabled();
     }
 
-    private void setupVoiceTranscription() {
-        CapabilityInfo capabilityInfo = Tasks.await(
-                Wearable.getCapabilityClient(context).getCapability(
-                        VOICE_TRANSCRIPTION_CAPABILITY_NAME, CapabilityClient.FILTER_REACHABLE));
+    public void recordButton_OnClick(View view)
+    {
+        Log.d("00001", "-----Record button has been clicked--------");
+        Log.d("00002", "getPhoneDeviceType(this)");
 
-        // capabilityInfo now has the reachable nodes(wear) w/ the transcription capability
-        updateTranscriptionCapability(capabilityInfo);
+        int phoneDevType = PhoneDeviceType.getPhoneDeviceType(getApplicationContext());
 
-        CapabilityClient.OnCapabilityChangedListener capabilityChangedListener =
-                capabilityInfo -> {
-                    updateTranscriptionCapability(capabilityInfo);
-                };
-        Wearable.getCapabilityClient(context).addListener(capabilityChangedListener,
-                VOICE_TRANSCRIPTION_CAPABILITY_NAME);
+        Log.d("00003", "Android device value is: " + PhoneDeviceType.DEVICE_TYPE_ANDROID);
+        Log.d("00004", "phoneDevType: " + phoneDevType);
     }
 
-    // Update the correct node to use for the transcription
-    private void updateTranscriptionCapability(CapabilityInfo capInfo) {
-        Set<Node> nodes = capInfo.getNodes();
-        transcriptionNodeID = getBestNodeID(nodes);
-    }
-
-    // Get the closest(best) node that is connected to the wearable
-    private String getBestNodeID(Set<Node> nodes) {
-        String bestNode = null;
-
-        for (Node node : nodes) {
-            // Find nearest node
-            if (node.isNearby()) {
-                return node.getId();
-            }
-        }
-        // If no nodes are connected
-        return null;
-    }
-
-    private void requestTranscription(byte[] voiceData) {
-        if (transcriptionNodeID != null) {
-            Task<Integer> sendTask = Wearable.getMessageClient(context).sendMessage(
-                    transcriptionNodeID, VOICE_TRANSCRIPTION_MESSAGE_PATH, voiceData);
-
-            // Add success listener
-            //sendTask.addOnSuccessListener();
-            // Add failure listener
-            //sendTask.addOnFailureListener();
-            // Or call Tasks.await() and catch ExecutionException
-        } else {
-            // Unable to retrieve node w/ transcription capability
-        }
-    }
 
     /* Within the Dashboard activity of the mobile app, add the message
      * received listener
-
-    @Override
-    public void onMessageReceived(MessageEvent messageEvent)
-    {
-        if (messageEvent.getPath().equals(VOICE_TRANSCRIPTION_MESSAGE_PATH))
-        {
-            Intent startIntent = new Intent(this, MainActivity.class);
-            startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startIntent.putExtra("VOICE_DATA", messageEvent.getData());
-            startActivity(startIntent);
-        }
-    }
-
     */
 }
