474b3e86a50225e55efa58a904e88d978f0c580c
Arthur Byra <arthur.h.byra@gmail.com>
2018-03-24 20:34:08 -0400

    Second attempt at committing changes to use local database for cardview

--- mobile/src/main/java/teammoodi/moodi/HistoryFragment.java
+++ mobile/src/main/java/teammoodi/moodi/HistoryFragment.java
@@ -50,7 +50,7 @@ public class HistoryFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
+     * @param param1 Parameter 1
      * @param param2 Parameter 2.
      * @return A new instance of fragment HistoryFragment.
      */
